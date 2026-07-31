8af388ff18afa86a5c70a0418277d6a767e570e6
Hassan Khosa <hassankhosa2122@gmail.com>
2026-07-31 21:27:59 +0500

    Fix connect-mongo v6 require interop

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 const path = require('node:path');
 const { initializeDatabase, createUser, verifyUser } = require('./database');
 
